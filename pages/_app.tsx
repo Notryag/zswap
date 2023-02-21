bcfa84ed20bf72fd03ccafe49b646eb7b8d9d7c7
tryag <40431305+Notryag@users.noreply.github.com>
2023-02-21 23:40:00 +0800

    upate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,26 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { createWeb3ReactRoot, useWeb3React, Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from "@ethersproject/providers";
 import Web3 from 'web3'
+import { NetworkContextName } from '@/config';
 
-function getLibrary(provider: any) {
-  return new Web3(provider) // this will vary according to whether you use e.g. ethers or web3.js
+export function getLibrary(provider: any): Web3Provider {
+  const library = new Web3Provider(provider);
+  library.pollingInterval = 15000;
+  return library;
 }
 
 
+const Web3ProviderNetwork = createWeb3ReactRoot(NetworkContextName)
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
-        <Component {...pageProps} />
+      <Web3ProviderNetwork getLibrary={getLibrary}>
+        <App />
+      </Web3ProviderNetwork>
     </Web3ReactProvider>
   )
 }
